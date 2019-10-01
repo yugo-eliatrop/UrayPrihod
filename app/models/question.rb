32b7710b6790f238b37c39c8f1e844b3a78b53ae
Ponomarev Sergei <papasergiusv@gmail.com>
2019-10-01 23:55:46 +0300

    Fix space under footer

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,10 +2,8 @@ class Question < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP }
-  # rubocop:disable Rails/InverseOf
   belongs_to :answerer, class_name: :user, foreign_key: :user_id,
                         optional: true
-  # rubocop:enable Rails/InverseOf
 
   scope :answered, -> { where.not(answer_text: nil) }
   scope :unanswered, -> { where(answer_text: nil) }
